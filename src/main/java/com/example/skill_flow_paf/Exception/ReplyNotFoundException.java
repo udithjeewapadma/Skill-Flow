5a134a8dddb521d01c71e2774e3e6106df0a4328
stgoshitha <wkgoshithapremachandra@gmail.com>
2025-04-10 14:22:24 +0530

    Add ReplyNotFoundException to handle missing reply cases

---
+++ src/main/java/com/example/skill_flow_paf/Exception/ReplyNotFoundException.java
@@ -0,0 +1,7 @@
+package com.example.skill_flow_paf.Exception;
+
+public class ReplyNotFoundException extends RuntimeException {
+    public ReplyNotFoundException(String message) {
+        super(message);
+    }
+}
