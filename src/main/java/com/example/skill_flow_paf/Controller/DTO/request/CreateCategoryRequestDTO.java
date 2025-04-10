4a176587cccdef50af4585802bb8c9c69b2fea98
Jeewapadma <udithjeewapadma117@gmail.com>
2025-04-10 09:01:27 +0530

    add validations to categoryrequestDTO

--- src/main/java/com/example/skill_flow_paf/Controller/DTO/request/CreateCategoryRequestDTO.java
+++ src/main/java/com/example/skill_flow_paf/Controller/DTO/request/CreateCategoryRequestDTO.java
@@ -2,12 +2,16 @@ package com.example.skill_flow_paf.Controller.DTO.request;
 
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
 @Data
 @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public class CreateCategoryRequestDTO {
 
+    @NotBlank(message = "Category name must not be blank")
+    @Pattern(regexp = "^[^\\d]*$", message = "Category name must not contain numbers")
     private String categoryName;
 
 }
