351841c5e3e0f6c0725d7c46d515da52b901c193
Jeewapadma <udithjeewapadma117@gmail.com>
2025-04-10 08:30:16 +0530

    implementing appControllerAdviser to handle exceptions

---
+++ src/main/java/com/example/skill_flow_paf/Exception/AppControllerAdviser.java
@@ -0,0 +1,7 @@
+package com.example.skill_flow_paf.Exception;
+
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class AppControllerAdviser {
+}
