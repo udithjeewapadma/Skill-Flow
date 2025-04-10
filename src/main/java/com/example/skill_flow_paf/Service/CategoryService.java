7c17d5e1f52aaaeea24afb3a7a157c16cfd88b6c
Jeewapadma <udithjeewapadma117@gmail.com>
2025-04-10 09:08:37 +0530

    implementin findCategoryById method in serviceImpl

--- src/main/java/com/example/skill_flow_paf/Service/CategoryService.java
+++ src/main/java/com/example/skill_flow_paf/Service/CategoryService.java
@@ -1,9 +1,12 @@
 package com.example.skill_flow_paf.Service;
 
 import com.example.skill_flow_paf.Controller.DTO.request.CreateCategoryRequestDTO;
+import com.example.skill_flow_paf.Exception.CategoryNotFoundException;
 import com.example.skill_flow_paf.Models.Category;
 
 public interface CategoryService {
 
     Category createCategory(CreateCategoryRequestDTO createCategoryRequestDTO);
+
+    Category findCategoryById(Long id) throws CategoryNotFoundException;
 }
--- src/main/java/com/example/skill_flow_paf/Service/Impl/CategoryServiceImpl.java
+++ src/main/java/com/example/skill_flow_paf/Service/Impl/CategoryServiceImpl.java
@@ -1,6 +1,7 @@
 package com.example.skill_flow_paf.Service.Impl;
 
 import com.example.skill_flow_paf.Controller.DTO.request.CreateCategoryRequestDTO;
+import com.example.skill_flow_paf.Exception.CategoryNotFoundException;
 import com.example.skill_flow_paf.Models.Category;
 import com.example.skill_flow_paf.Repository.CategoryRepository;
 import com.example.skill_flow_paf.Service.CategoryService;
@@ -18,5 +19,11 @@ public class CategoryServiceImpl implements CategoryService {
         Category category = new Category();
         category.setCategoryName(createCategoryRequestDTO.getCategoryName());
         return categoryRepository.save(category);
+    }
+
+    @Override
+    public Category findCategoryById(Long id) throws CategoryNotFoundException {
+        return categoryRepository.findById(id)
+                .orElseThrow(() -> new CategoryNotFoundException("Category not found"));
     }
 }
