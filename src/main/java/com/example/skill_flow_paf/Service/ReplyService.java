d88757956455c45a8ceb0947ab6fb87d03f94710
stgoshitha <wkgoshithapremachandra@gmail.com>
2025-04-10 14:13:51 +0530

    Create ReplyService interface and implement createReply method

---
+++ src/main/java/com/example/skill_flow_paf/Service/ReplyService.java
@@ -0,0 +1,8 @@
+package com.example.skill_flow_paf.Service;
+
+import com.example.skill_flow_paf.Controller.DTO.request.CreateReplyRequestDTO;
+import com.example.skill_flow_paf.Models.Reply;
+
+public interface ReplyService {
+    Reply createReply(Long helpDeskId, CreateReplyRequestDTO createReplyRequestDTO);
+}
