a4bed85a98f95643714b57bf1d337ff466aac0d7
stgoshitha <wkgoshithapremachandra@gmail.com>
2025-04-10 10:35:31 +0530

    Create HelpDeskRepository file

---
+++ src/main/java/com/example/skill_flow_paf/Repository/HelpDeskRepository.java
@@ -0,0 +1,7 @@
+package com.example.skill_flow_paf.Repository;
+
+import com.example.skill_flow_paf.Models.HelpDesk;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface HelpDeskRepository extends JpaRepository<HelpDesk, Long> {
+}
