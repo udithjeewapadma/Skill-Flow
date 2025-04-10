cd29d6ba4b86f503bedb734968846a7e7a46a10b
stgoshitha <wkgoshithapremachandra@gmail.com>
2025-04-10 10:37:57 +0530

    Create exception for help desk

---
+++ src/main/java/com/example/skill_flow_paf/Exception/HelpDeskNotFoundException.java
@@ -0,0 +1,7 @@
+package com.example.skill_flow_paf.Exception;
+
+public class HelpDeskNotFoundException extends RuntimeException {
+    public HelpDeskNotFoundException(String message) {
+        super(message);
+    }
+}
