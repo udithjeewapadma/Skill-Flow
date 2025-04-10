726ced9dcccaba9e1b7a76d1c358745ba88af1a9
Jeewapadma <udithjeewapadma117@gmail.com>
2025-04-10 09:26:23 +0530

    implementing findAllCategories in serviceImpl

--- src/main/java/com/example/skill_flow_paf/Service/CategoryService.java
+++ src/main/java/com/example/skill_flow_paf/Service/CategoryService.java
@@ -1,12 +1,17 @@
 package com.example.skill_flow_paf.Service;
 
 import com.example.skill_flow_paf.Controller.DTO.request.CreateCategoryRequestDTO;
+import com.example.skill_flow_paf.Controller.DTO.response.CategoryResponseDTO;
 import com.example.skill_flow_paf.Exception.CategoryNotFoundException;
 import com.example.skill_flow_paf.Models.Category;
+
+import java.util.List;
 
 public interface CategoryService {
 
     Category createCategory(CreateCategoryRequestDTO createCategoryRequestDTO);
 
     Category findCategoryById(Long id) throws CategoryNotFoundException;
+
+     List<CategoryResponseDTO> findAllCategories();
 }
--- src/main/java/com/example/skill_flow_paf/Service/Impl/CategoryServiceImpl.java
+++ src/main/java/com/example/skill_flow_paf/Service/Impl/CategoryServiceImpl.java
@@ -1,12 +1,18 @@
 package com.example.skill_flow_paf.Service.Impl;
 
 import com.example.skill_flow_paf.Controller.DTO.request.CreateCategoryRequestDTO;
+import com.example.skill_flow_paf.Controller.DTO.response.CategoryResponseDTO;
 import com.example.skill_flow_paf.Exception.CategoryNotFoundException;
 import com.example.skill_flow_paf.Models.Category;
 import com.example.skill_flow_paf.Repository.CategoryRepository;
 import com.example.skill_flow_paf.Service.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+
+import java.util.List;
+import java.util.stream.Collectors;
+
 
 @Service
 public class CategoryServiceImpl implements CategoryService {
@@ -26,4 +32,17 @@ public class CategoryServiceImpl implements CategoryService {
         return categoryRepository.findById(id)
                 .orElseThrow(() -> new CategoryNotFoundException("Category not found"));
     }
+
+    @Override
+    public List<CategoryResponseDTO> findAllCategories() {
+        List<Category> categories = categoryRepository.findAll();
+        return categories.stream().map(category -> {
+            CategoryResponseDTO categoryResponseDTO = new CategoryResponseDTO();
+            categoryResponseDTO.setId(category.getId());
+            categoryResponseDTO.setCategoryName(category.getCategoryName());
+            return categoryResponseDTO;
+        }).collect(Collectors.toList());
+    }
+
+
 }
