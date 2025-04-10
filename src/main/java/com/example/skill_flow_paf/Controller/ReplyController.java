656147b146ce83e56cfd68ec283525d4f8d3da30
stgoshitha <wkgoshithapremachandra@gmail.com>
2025-04-10 15:23:21 +0530

    Create post mapping for create reply

--- src/main/java/com/example/skill_flow_paf/Controller/ReplyController.java
+++ src/main/java/com/example/skill_flow_paf/Controller/ReplyController.java
@@ -1,4 +1,31 @@
 package com.example.skill_flow_paf.Controller;
 
+import com.example.skill_flow_paf.Controller.DTO.request.CreateReplyRequestDTO;
+import com.example.skill_flow_paf.Controller.DTO.response.ReplyResponseDTO;
+import com.example.skill_flow_paf.Models.Reply;
+import com.example.skill_flow_paf.Models.User;
+import com.example.skill_flow_paf.Service.ReplyService;
+import com.example.skill_flow_paf.Service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/reply")
 public class ReplyController {
+
+    @Autowired
+    private ReplyService replyService;
+
+    @PostMapping
+    public ReplyResponseDTO createReply(@RequestParam Long userId, @RequestParam Long helpDeskId, @RequestBody CreateReplyRequestDTO createReplyRequestDTO){
+        Reply reply = replyService.createReply(userId, helpDeskId, createReplyRequestDTO);
+
+        ReplyResponseDTO replyResponseDTO = new ReplyResponseDTO();
+        replyResponseDTO.setReplyText(reply.getReplyText());
+        replyResponseDTO.setId(reply.getId());
+        replyResponseDTO.setHelpDeskId(reply.getHelpDesk().getId());
+        replyResponseDTO.setUserId(reply.getUser().getId());
+
+        return replyResponseDTO;
+    }
 }
