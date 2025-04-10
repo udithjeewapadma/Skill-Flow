2b5c82b1ad53a5fd21c168b2d6462a2e9fd7f49c
stgoshitha <wkgoshithapremachandra@gmail.com>
2025-04-10 11:27:23 +0530

    Implement help desk response DTO file

---
+++ src/main/java/com/example/skill_flow_paf/Controller/DTO/response/HelpDeskResponseDTO.java
@@ -0,0 +1,10 @@
+package com.example.skill_flow_paf.Controller.DTO.response;
+
+import lombok.Data;
+
+@Data
+public class HelpDeskResponseDTO {
+    private Long id;
+    private String question;
+    private Long userId;
+}
