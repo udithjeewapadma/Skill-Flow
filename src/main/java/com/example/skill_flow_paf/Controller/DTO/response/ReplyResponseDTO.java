34231ddbcc22d6f9c1136276ca2c8785576f6748
stgoshitha <wkgoshithapremachandra@gmail.com>
2025-04-10 14:27:56 +0530

    Add  ReplyResponseDTO for returning reply

---
+++ src/main/java/com/example/skill_flow_paf/Controller/DTO/response/ReplyResponseDTO.java
@@ -0,0 +1,10 @@
+package com.example.skill_flow_paf.Controller.DTO.response;
+
+import lombok.Data;
+
+@Data
+public class ReplyResponseDTO {
+    private Long id;
+    private String replyText;
+    private Long helpDeskId;
+}
