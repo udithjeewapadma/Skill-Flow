bb51d22077eee585a4827e189cf72ec2a6e55d66
Jeewapadma <udithjeewapadma117@gmail.com>
2025-04-10 08:44:20 +0530

    implementing categoryResponseDTO

---
+++ src/main/java/com/example/skill_flow_paf/Controller/DTO/response/CategoryResponseDTO.java
@@ -0,0 +1,10 @@
+package com.example.skill_flow_paf.Controller.DTO.response;
+
+import lombok.Data;
+
+@Data
+public class CategoryResponseDTO {
+
+    private Long id;
+    private String categoryName;
+}
