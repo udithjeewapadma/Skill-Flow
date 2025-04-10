100d44509c5e75f5f568e6b94697e8cdc510c644
Jeewapadma <udithjeewapadma117@gmail.com>
2025-04-10 08:42:38 +0530

    implementing createCategory method in serviceImpl

---
+++ src/main/java/com/example/skill_flow_paf/Service/Impl/CategoryServiceImpl.java
@@ -0,0 +1,22 @@
+package com.example.skill_flow_paf.Service.Impl;
+
+import com.example.skill_flow_paf.Controller.DTO.request.CreateCategoryRequestDTO;
+import com.example.skill_flow_paf.Models.Category;
+import com.example.skill_flow_paf.Repository.CategoryRepository;
+import com.example.skill_flow_paf.Service.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CategoryServiceImpl implements CategoryService {
+
+    @Autowired
+    private  CategoryRepository categoryRepository;
+
+    @Override
+    public Category createCategory(CreateCategoryRequestDTO createCategoryRequestDTO) {
+        Category category = new Category();
+        category.setCategoryName(createCategoryRequestDTO.getCategoryName());
+        return categoryRepository.save(category);
+    }
+}
