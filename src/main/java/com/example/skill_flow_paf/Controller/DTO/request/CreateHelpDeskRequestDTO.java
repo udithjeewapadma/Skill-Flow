9dbab402a8f3f8f47d1a08119f4bbc1df62b460e
stgoshitha <wkgoshithapremachandra@gmail.com>
2025-04-10 16:39:01 +0530

    Add JsonNaming annotation

--- src/main/java/com/example/skill_flow_paf/Controller/DTO/request/CreateHelpDeskRequestDTO.java
+++ src/main/java/com/example/skill_flow_paf/Controller/DTO/request/CreateHelpDeskRequestDTO.java
@@ -1,10 +1,13 @@
 package com.example.skill_flow_paf.Controller.DTO.request;
 
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public class CreateHelpDeskRequestDTO {
 
     @NotBlank(message = "Question must not be empty")
