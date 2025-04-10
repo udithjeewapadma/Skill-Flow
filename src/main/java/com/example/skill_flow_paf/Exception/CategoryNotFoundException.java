dea805ceb274d974e7c0a672209b11f0e5edc642
Jeewapadma <udithjeewapadma117@gmail.com>
2025-04-10 08:31:20 +0530

    implementing categoryNotFoundException

---
+++ src/main/java/com/example/skill_flow_paf/Exception/CategoryNotFoundException.java
@@ -0,0 +1,7 @@
+package com.example.skill_flow_paf.Exception;
+
+public class CategoryNotFoundException extends RuntimeException {
+  public CategoryNotFoundException(String message) {
+    super(message);
+  }
+}
