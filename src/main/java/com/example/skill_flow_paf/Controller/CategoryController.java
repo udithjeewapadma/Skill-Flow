ea6d7dfd3876c8aad3b1b5aa72fa431fec89fb50
Jeewapadma <udithjeewapadma117@gmail.com>
2025-04-10 09:16:39 +0530

    implementing getMapping to getCategoryById

--- src/main/java/com/example/skill_flow_paf/Controller/CategoryController.java
+++ src/main/java/com/example/skill_flow_paf/Controller/CategoryController.java
@@ -2,14 +2,12 @@ package com.example.skill_flow_paf.Controller;
 
 import com.example.skill_flow_paf.Controller.DTO.request.CreateCategoryRequestDTO;
 import com.example.skill_flow_paf.Controller.DTO.response.CategoryResponseDTO;
+import com.example.skill_flow_paf.Exception.CategoryNotFoundException;
 import com.example.skill_flow_paf.Models.Category;
 import com.example.skill_flow_paf.Service.CategoryService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/categories")
@@ -21,6 +19,15 @@ public class CategoryController {
     @PostMapping
     private CategoryResponseDTO createCategory(@RequestBody @Valid CreateCategoryRequestDTO createCategoryRequestDTO) {
         Category category = categoryService.createCategory(createCategoryRequestDTO);
+        CategoryResponseDTO categoryResponseDTO = new CategoryResponseDTO();
+        categoryResponseDTO.setId(category.getId());
+        categoryResponseDTO.setCategoryName(category.getCategoryName());
+        return categoryResponseDTO;
+    }
+
+    @GetMapping("/{category-id}")
+    public CategoryResponseDTO getCategoryById(@PathVariable("category-id") Long id) throws CategoryNotFoundException {
+        Category category = categoryService.findCategoryById(id);
         CategoryResponseDTO categoryResponseDTO = new CategoryResponseDTO();
         categoryResponseDTO.setId(category.getId());
         categoryResponseDTO.setCategoryName(category.getCategoryName());
