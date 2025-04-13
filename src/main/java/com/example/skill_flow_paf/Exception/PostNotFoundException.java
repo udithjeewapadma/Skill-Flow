c205888f23644ffadc33f004e3d9f62b7627e68e
Jeewapadma <udithjeewapadma117@gmail.com>
2025-04-13 18:59:30 +0530

    implementing PostNotFoundException exception

---
+++ src/main/java/com/example/skill_flow_paf/Exception/PostNotFoundException.java
@@ -0,0 +1,7 @@
+package com.example.skill_flow_paf.Exception;
+
+public class PostNotFoundException extends RuntimeException {
+    public PostNotFoundException(String message) {
+        super(message);
+    }
+}
