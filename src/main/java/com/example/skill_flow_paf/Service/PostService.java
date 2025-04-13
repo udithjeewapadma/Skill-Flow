8eab7e97066619a060e9715ba4d600b44b328fcb
Jeewapadma <udithjeewapadma117@gmail.com>
2025-04-13 19:30:32 +0530

    implementing createPost method in postServiceImpl

--- src/main/java/com/example/skill_flow_paf/Service/PostService.java
+++ src/main/java/com/example/skill_flow_paf/Service/PostService.java
@@ -4,7 +4,9 @@ import com.example.skill_flow_paf.Controller.DTO.request.CreatePostRequestDTO;
 import com.example.skill_flow_paf.Controller.DTO.response.PostResponseDTO;
 import com.example.skill_flow_paf.Models.Post;
 
+import java.io.IOException;
+
 public interface PostService {
 
-    PostResponseDTO createPost(Long userId, Long categoryId, CreatePostRequestDTO createPostRequestDTO);
+    PostResponseDTO createPost(Long userId, Long categoryId, CreatePostRequestDTO createPostRequestDTO) throws IOException;
 }
