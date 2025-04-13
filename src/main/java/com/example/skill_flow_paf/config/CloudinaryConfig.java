7ac8c12ba75774c014b1e903eadedddda61a70bb
Jeewapadma <udithjeewapadma117@gmail.com>
2025-04-13 19:18:47 +0530

    implementing cloudinary config class to manage images

---
+++ src/main/java/com/example/skill_flow_paf/config/CloudinaryConfig.java
@@ -0,0 +1,26 @@
+package com.example.skill_flow_paf.Config;
+
+import com.cloudinary.Cloudinary;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+public class CloudinaryConfig {
+
+    private final String CLOUD_NAME = "skillFlow";
+    private final String API_KEY = "427293554287241";
+    private final String API_SECRET = "Ym-WExjGUZMOMHUzcBzcqEVleCY";
+
+    @Bean
+    public Cloudinary cloudinary(){
+        Map<String,String> config = new HashMap<>();
+        config.put("cloud_name",CLOUD_NAME);
+        config.put("api_key",API_KEY);
+        config.put("api_secret",API_SECRET);
+
+        return new Cloudinary(config);
+    }
+}
