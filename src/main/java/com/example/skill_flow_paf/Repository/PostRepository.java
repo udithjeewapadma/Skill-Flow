37ec29b4d7144592a77702dc0c68230b9ab5a8dd
Jeewapadma <udithjeewapadma117@gmail.com>
2025-04-13 19:00:41 +0530

    implementing postRepository interface

---
+++ src/main/java/com/example/skill_flow_paf/Repository/PostRepository.java
@@ -0,0 +1,7 @@
+package com.example.skill_flow_paf.Repository;
+
+import com.example.skill_flow_paf.Models.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostRepository extends JpaRepository<Post, Long> {
+}
