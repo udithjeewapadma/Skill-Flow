10f89e3abe22a3ee87e082a137f9022df6858096
Jeewapadma <udithjeewapadma117@gmail.com>
2025-04-14 09:55:50 +0530

    implementing getAllPosts method in postController

--- src/main/java/com/example/skill_flow_paf/Controller/PostController.java
+++ src/main/java/com/example/skill_flow_paf/Controller/PostController.java
@@ -9,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
+import java.util.List;
 
 @RestController
 @RequestMapping("/posts")
@@ -22,5 +23,10 @@ public class PostController {
                                                       @RequestParam Long categoryId,
                                                       @ModelAttribute @Valid CreatePostRequestDTO requestDTO) throws IOException {
         return ResponseEntity.ok(postService.createPost(categoryId,userId,requestDTO));
+    }
+
+    @GetMapping
+    public List<PostResponseDTO> getAllPosts() {
+        return postService.getAllPosts();
     }
 }
