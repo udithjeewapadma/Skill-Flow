8acb6daa37e4867fef42cd14ffdd79323b2ccf2f
Jeewapadma <udithjeewapadma117@gmail.com>
2025-04-14 09:48:31 +0530

    add validations

--- src/main/java/com/example/skill_flow_paf/Controller/DTO/request/CreatePostRequestDTO.java
+++ src/main/java/com/example/skill_flow_paf/Controller/DTO/request/CreatePostRequestDTO.java
@@ -1,5 +1,6 @@
 package com.example.skill_flow_paf.Controller.DTO.request;
 
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 import org.springframework.web.multipart.MultipartFile;
@@ -9,10 +10,10 @@ import java.util.List;
 @Data
 public class CreatePostRequestDTO {
 
-    @NotNull
+    @NotBlank(message = "Title is required")
     private String title;
 
-    @NotNull
+    @NotBlank(message = "Description is required")
     private String description;
 
     private List<MultipartFile> imageFiles;
