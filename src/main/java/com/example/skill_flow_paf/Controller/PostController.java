d6fe8d41e7868ce811e02c6df2805d621e636f07
Jeewapadma <udithjeewapadma117@gmail.com>
2025-04-14 09:48:31 +0530

    add validations

--- src/main/java/com/example/skill_flow_paf/Controller/PostController.java
+++ src/main/java/com/example/skill_flow_paf/Controller/PostController.java
@@ -3,6 +3,7 @@ package com.example.skill_flow_paf.Controller;
 import com.example.skill_flow_paf.Controller.DTO.request.CreatePostRequestDTO;
 import com.example.skill_flow_paf.Controller.DTO.response.PostResponseDTO;
 import com.example.skill_flow_paf.Service.PostService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -17,9 +18,9 @@ public class PostController {
     private PostService postService;
 
     @PostMapping
-    public ResponseEntity<PostResponseDTO> createPost(@RequestParam Long userId,
+    public ResponseEntity<PostResponseDTO> createPost( @RequestParam Long userId,
                                                       @RequestParam Long categoryId,
-                                                      @ModelAttribute CreatePostRequestDTO requestDTO) throws IOException {
+                                                      @ModelAttribute @Valid CreatePostRequestDTO requestDTO) throws IOException {
         return ResponseEntity.ok(postService.createPost(categoryId,userId,requestDTO));
     }
 }
