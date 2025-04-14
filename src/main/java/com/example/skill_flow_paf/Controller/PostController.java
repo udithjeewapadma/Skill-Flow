9ab320af0a7537dbff619017513f7731d58c275c
Jeewapadma <udithjeewapadma117@gmail.com>
2025-04-14 10:01:56 +0530

    implementing getPostById method in postController

--- src/main/java/com/example/skill_flow_paf/Controller/PostController.java
+++ src/main/java/com/example/skill_flow_paf/Controller/PostController.java
@@ -2,6 +2,8 @@ package com.example.skill_flow_paf.Controller;
 
 import com.example.skill_flow_paf.Controller.DTO.request.CreatePostRequestDTO;
 import com.example.skill_flow_paf.Controller.DTO.response.PostResponseDTO;
+import com.example.skill_flow_paf.Exception.PostNotFoundException;
+import com.example.skill_flow_paf.Models.Post;
 import com.example.skill_flow_paf.Service.PostService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -28,5 +30,10 @@ public class PostController {
     @GetMapping
     public List<PostResponseDTO> getAllPosts() {
         return postService.getAllPosts();
+    }
+
+    @GetMapping("/{post-id}")
+    public PostResponseDTO getPostById(@PathVariable("post-id") Long postId) throws PostNotFoundException {
+        return postService.findById(postId);
     }
 }
