bd1767a3853b2d9271ca1c785f772d5c18e2330a
tharushika2002 <sttharuu123@gmail.com>
2025-04-23 18:20:50 +0530

    change minimum size of question

--- src/main/java/com/example/skill_flow_paf/Controller/DTO/request/CreateHelpDeskRequestDTO.java
+++ src/main/java/com/example/skill_flow_paf/Controller/DTO/request/CreateHelpDeskRequestDTO.java
@@ -11,6 +11,6 @@ import lombok.Data;
 public class CreateHelpDeskRequestDTO {
 
     @NotBlank(message = "Question must not be empty")
-    @Size(min = 5, max = 1000, message = "Question must be between 5 and 1000 characters")
+    @Size(min = 10, max = 1000, message = "Question must be between 5 and 1000 characters")
     private String question;
 }
