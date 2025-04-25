21b440037cf7bdd8dcdfff4a67fd9063c7978f28
Pasindu Madushan <pasindumadushanr01@gmail.com>
2025-04-25 19:25:46 +0530

    change the code:add Id

--- src/main/java/com/example/skill_flow_paf/Controller/DTO/response/LearningPlanResponseDTO.java
+++ src/main/java/com/example/skill_flow_paf/Controller/DTO/response/LearningPlanResponseDTO.java
@@ -7,6 +7,7 @@ import java.util.List;
 @Data
 public class LearningPlanResponseDTO {
 
+    private Long id;
     private String title;
     private String description;
     private List<String> resources;
---
+++ src/main/java/com/example/skill_flow_paf/Controller/LearningPlanController.java
@@ -0,0 +1,4 @@
+package com.example.skill_flow_paf.Controller;
+
+public class LearningPlanController {
+}
