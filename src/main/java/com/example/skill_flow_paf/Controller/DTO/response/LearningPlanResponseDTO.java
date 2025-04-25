722ab7ef84f65d553d7352d0ba66fbdb7fda7330
Pasindu Madushan <pasindumadushanr01@gmail.com>
2025-04-25 18:41:37 +0530

    Implement Respone DTO for LearningPlan

---
+++ src/main/java/com/example/skill_flow_paf/Controller/DTO/response/LearningPlanResponseDTO.java
@@ -0,0 +1,16 @@
+package com.example.skill_flow_paf.Controller.DTO.response;
+
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class LearningPlanResponseDTO {
+
+    private String title;
+    private String description;
+    private List<String> resources;
+    private String timeLine;
+    private List<Long> postIds;
+    private int progress;
+}
