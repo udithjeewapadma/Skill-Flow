dfe580c876687281e335f01ae1f339faf091b318
tharushika2002 <sttharuu123@gmail.com>
2025-04-25 15:13:41 +0530

    Add createComment method

--- src/main/java/com/example/skill_flow_paf/Service/CommentService.java
+++ src/main/java/com/example/skill_flow_paf/Service/CommentService.java
@@ -1,4 +1,7 @@
 package com.example.skill_flow_paf.Service;
 
+import com.example.skill_flow_paf.Controller.DTO.request.CreateCommentRequestDTO;
+
 public interface CommentService {
+    CommentService createComment(Long commentId, CreateCommentRequestDTO createCommentRequestDTO);
 }
