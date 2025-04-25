138bd25688d172f40971f0af686d98690819f454
Pasindu Madushan <pasindumadushanr01@gmail.com>
2025-04-25 18:56:21 +0530

    Create Exception for LearningPlan

---
+++ src/main/java/com/example/skill_flow_paf/Exception/LearningPlanNotFoundException.java
@@ -0,0 +1,7 @@
+package com.example.skill_flow_paf.Exception;
+
+public class LearningPlanNotFoundException extends RuntimeException {
+    public LearningPlanNotFoundException(String message) {
+        super(message);
+    }
+}
