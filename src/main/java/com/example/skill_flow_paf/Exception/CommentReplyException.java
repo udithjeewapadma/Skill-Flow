cae9a9c2b9a95ebd58c0aec50c58ed5690133cb6
tharushika2002 <sttharuu123@gmail.com>
2025-04-26 11:16:38 +0530

    create CommentReplyException exception

---
+++ src/main/java/com/example/skill_flow_paf/Exception/CommentReplyException.java
@@ -0,0 +1,7 @@
+package com.example.skill_flow_paf.Exception;
+
+public class CommentReplyException extends RuntimeException {
+    public CommentReplyException(String message) {
+        super(message);
+    }
+}
