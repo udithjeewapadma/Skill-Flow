00c53a91362897f0569396dce2956420275ea078
Pasindu Madushan <pasindumadushanr01@gmail.com>
2025-04-26 15:18:59 +0530

    implementing findById method in learningPlanServiceImpl

--- src/main/java/com/example/skill_flow_paf/Service/Impl/LearningPlanServiceImpl.java
+++ src/main/java/com/example/skill_flow_paf/Service/Impl/LearningPlanServiceImpl.java
@@ -1,6 +1,8 @@
 package com.example.skill_flow_paf.Service.Impl;
 
 import com.example.skill_flow_paf.Controller.DTO.request.CreateLearningPlanRequestDTO;
+import com.example.skill_flow_paf.Controller.DTO.response.LearningPlanResponseDTO;
+import com.example.skill_flow_paf.Exception.LearningPlanNotFoundException;
 import com.example.skill_flow_paf.Exception.PostNotFoundException;
 import com.example.skill_flow_paf.Exception.UserNotFoundExecption;
 import com.example.skill_flow_paf.Models.LearningPlan;
@@ -13,6 +15,7 @@ import com.example.skill_flow_paf.Service.LearningPlanService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Collections;
 import java.util.List;
 
 @Service
@@ -53,7 +56,28 @@ public class LearningPlanServiceImpl implements LearningPlanService {
         return learningPlanRepository.save(learningPlan);
     }
 
+    @Override
+    public LearningPlanResponseDTO findById(Long id) throws LearningPlanNotFoundException {
 
+        LearningPlan learningPlan = learningPlanRepository.findById(id)
+                .orElseThrow(() -> new LearningPlanNotFoundException("Learning plan not found with id: " + id));
+
+        LearningPlanResponseDTO responseDTO = new LearningPlanResponseDTO();
+        responseDTO.setId(learningPlan.getId());
+        responseDTO.setTitle(learningPlan.getTitle());
+        responseDTO.setDescription(learningPlan.getDescription());
+        responseDTO.setResources(
+                learningPlan.getResources() != null ? List.of(learningPlan.getResources().split(",")) : Collections.emptyList()
+        );
+        responseDTO.setTimeLine(learningPlan.getTimeLine());
+        responseDTO.setProgress(learningPlan.getProgress());
+
+        responseDTO.setPostIds(learningPlan.getPosts().stream()
+                .map(post -> post.getId())
+                .toList());
+
+        return responseDTO;
+    }
 
 
 }
--- src/main/java/com/example/skill_flow_paf/Service/LearningPlanService.java
+++ src/main/java/com/example/skill_flow_paf/Service/LearningPlanService.java
@@ -1,10 +1,14 @@
 package com.example.skill_flow_paf.Service;
 
 import com.example.skill_flow_paf.Controller.DTO.request.CreateLearningPlanRequestDTO;
+import com.example.skill_flow_paf.Controller.DTO.response.LearningPlanResponseDTO;
 import com.example.skill_flow_paf.Models.LearningPlan;
 
 import java.util.List;
 
 public interface LearningPlanService {
     LearningPlan createLearningPlan(Long userId, CreateLearningPlanRequestDTO createLearningPlanRequestDTO);
+
+    LearningPlanResponseDTO findById(Long id);
+
 }
