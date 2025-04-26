2441cfbfdae9c388effb0d32b31c2150e8a2e1a6
tharushika2002 <sttharuu123@gmail.com>
2025-04-26 11:19:51 +0530

    implementing create reply method

---
+++ src/main/java/com/example/skill_flow_paf/Service/CommentReplyService.java
@@ -0,0 +1,8 @@
+package com.example.skill_flow_paf.Service;
+
+import com.example.skill_flow_paf.Controller.DTO.request.CreateCommentReplyRequestDTO;
+import com.example.skill_flow_paf.Models.CommentReply;
+
+public interface CommentReplyService {
+    CommentReply createCommentReply(Long commentId , CreateCommentReplyRequestDTO createCommentReplyRequestDTO);
+}
