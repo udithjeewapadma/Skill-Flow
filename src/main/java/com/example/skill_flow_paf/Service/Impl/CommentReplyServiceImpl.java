c2b97f7874cb9af7abad4f45a71420d4577d7aad
tharushika2002 <sttharuu123@gmail.com>
2025-04-26 12:10:32 +0530

    implement find by id method in CommentReplyServiceImpl

--- src/main/java/com/example/skill_flow_paf/Service/Impl/CommentReplyServiceImpl.java
+++ src/main/java/com/example/skill_flow_paf/Service/Impl/CommentReplyServiceImpl.java
@@ -1,7 +1,9 @@
 package com.example.skill_flow_paf.Service.Impl;
 
 import com.example.skill_flow_paf.Controller.DTO.request.CreateCommentReplyRequestDTO;
+import com.example.skill_flow_paf.Controller.DTO.response.CommentReplyResponseDTO;
 import com.example.skill_flow_paf.Exception.CommentNotFoundException;
+import com.example.skill_flow_paf.Exception.CommentReplyException;
 import com.example.skill_flow_paf.Models.Comment;
 import com.example.skill_flow_paf.Models.CommentReply;
 import com.example.skill_flow_paf.Repository.CommentReplyRepository;
@@ -26,6 +28,18 @@ public class CommentReplyServiceImpl implements CommentReplyService {
         commentReply.setComment(comment);
 
         return commentReplyRepository.save(commentReply);
+    }
+
+    @Override
+    public CommentReplyResponseDTO findById(Long id) {
+        CommentReply commentReply = commentReplyRepository.findById(id).orElseThrow(()->new CommentReplyException("comment reply id is not found"));
+        CommentReplyResponseDTO commentReplyResponseDTO = new CommentReplyResponseDTO();
+
+        commentReplyResponseDTO.setCommentId(commentReply.getComment().getId());
+        commentReplyResponseDTO.setId(commentReply.getId());
+        commentReplyResponseDTO.setReplyBody(commentReply.getReplyBody());
+
+        return commentReplyResponseDTO;
     }
 
 
