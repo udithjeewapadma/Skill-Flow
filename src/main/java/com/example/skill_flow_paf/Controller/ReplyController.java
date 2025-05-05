b776a0f7f9adb34d6404ba22337b31d8dcdbedd4
tharushika2002 <wkgoshithapremachandra@gmail.com>
2025-05-05 15:18:29 +0530

    change enpoint of replycontroller

--- src/main/java/com/example/skill_flow_paf/Controller/ReplyController.java
+++ src/main/java/com/example/skill_flow_paf/Controller/ReplyController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/reply")
+@RequestMapping("/replies")
 public class ReplyController {
 
     @Autowired
