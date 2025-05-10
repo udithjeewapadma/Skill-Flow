32e02ef415a95aa0820dc24b1e852625c29abd59
stgoshitha <wkgoshithapremachandra@gmail.com>
2025-05-10 14:59:06 +0530

    Revert "Add findByHelpDeskId method in service interface"
    This reverts commit 6c245cd5351b5510138f1ebf155e2bc4602999a5.

--- src/main/java/com/example/skill_flow_paf/Repository/ReplyRepository.java
+++ src/main/java/com/example/skill_flow_paf/Repository/ReplyRepository.java
@@ -3,8 +3,5 @@ package com.example.skill_flow_paf.Repository;
 import com.example.skill_flow_paf.Models.Reply;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
-
 public interface ReplyRepository extends JpaRepository<Reply, Long> {
-    List<Reply> findByHelpDeskId(Long helpDeskId);
 }
