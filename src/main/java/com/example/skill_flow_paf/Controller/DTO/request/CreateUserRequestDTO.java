aacbc50739db15e02ca06cbb87bc4a64e6c5ba12
stgoshitha <wkgoshithapremachandra@gmail.com>
2025-05-10 15:02:59 +0530

    Reapply "complete user login and signup with OAuth2"
    This reverts commit b92bc798ba72dddf63ce4defad3a70eb850cb27a.

--- src/main/java/com/example/skill_flow_paf/Controller/DTO/request/CreateUserRequestDTO.java
+++ src/main/java/com/example/skill_flow_paf/Controller/DTO/request/CreateUserRequestDTO.java
@@ -12,21 +12,23 @@ import lombok.Data;
 @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public class CreateUserRequestDTO {
 
-    @NotBlank(message = "Username is required")
+//    @NotBlank(message = "Username is required")
     @Size(min = 3, max = 50, message = "Username must be between 3 and 50 characters")
     private String username;
 
     @Size(max = 255, message = "About section cannot exceed 255 characters")
     private String about;
 
-    @NotBlank(message = "Phone number is required")
+//    @NotBlank(message = "Phone number is required")
     @Pattern(regexp = "^\\d{10}$", message = "Phone number must be exactly 10 digits")
     private String phoneNumber;
 
     @Size(max = 255, message = "Qualifications section cannot exceed 255 characters")
     private String qualifications;
 
-    @NotBlank(message = "Email is required")
+    //@NotBlank(message = "Email is required")
     @Email(message = "Invalid email format")
     private String email;
+
+    private String role;
 }
