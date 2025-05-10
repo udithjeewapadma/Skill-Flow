af48e6b818865504c1e47860efc5ee92dca92c5e
stgoshitha <wkgoshithapremachandra@gmail.com>
2025-05-10 15:03:17 +0530

    Revert "complete user login and signup with OAuth2"
    This reverts commit f6a2260aa6da083b7427b0863003d1763e8fcd58.

--- src/main/java/com/example/skill_flow_paf/Repository/UserRepository.java
+++ src/main/java/com/example/skill_flow_paf/Repository/UserRepository.java
@@ -3,8 +3,5 @@ package com.example.skill_flow_paf.Repository;
 import com.example.skill_flow_paf.Models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
-
 public interface UserRepository extends JpaRepository<User, Long> {
-    Optional<User> findByEmail(String email);
 }
